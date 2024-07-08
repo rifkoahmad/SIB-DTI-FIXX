f7811d79e0b769a16aeecde4f699340d533748c6
rifkoahamad <kasperold4@gmail.com>
2024-07-09 02:45:55 +0700

    FIX COMMIT

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'users_id',
+        'prodi_id',
+        'nama',
+        'nim',
+        'angkatan',
+        'ipk',
+    ];
+    protected $table = 'mahasiswas';
+    protected $primaryKey = 'id';
+
+    public function prodis(): BelongsTo
+    {
+        return $this->belongsTo(Prodi::class, 'prodi_id');
+    }
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'users_id');
+    }
+}
---
+++ app/Models/Pegawai.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Pegawai extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['users_id', 'nama', 'nip_nik', 'jabatan_akademik', 'no_telepon', 'email', 'foto'];
+
+
+    protected $table = 'pegawais';
+    protected $primaryKey = 'id';
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+    public function peminjamen()
+    {
+        return $this->hasMany(Peminjaman::class, 'pegawais_id');
+    }
+
+    public function pengembalians()
+{
+    return $this->hasMany(Pengembalian::class, 'pegawais_id');
+}
+
+}
